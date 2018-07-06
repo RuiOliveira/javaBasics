f3c77e42d27299b38352de261c072d9a41c0d45a
Rui Oliveira <rui.m.oliveira@mindera.com>
2018-07-06 22:57:24 +0100

    java multidimentional arrays

---
+++ src/com/example/basics/Basics/MultidimensionalArrays.java
@@ -0,0 +1,15 @@
+package com.example.basics.Basics;
+
+public class MultidimensionalArrays {
+
+    public static void main(String[] args) {
+
+        int b[][] = {{2,4,5},{3,4,7},{5,2,1}};
+
+        for(int i = 0; i<b.length; i++){
+            for(int j = 0; j<b[i].length; j++){
+                System.out.println(b[i][j] + "");
+            }
+        }
+    }
+}
